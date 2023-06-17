564dd85b962f14c6e20bd4d9a6dc60c0d9241e2b
sergey <sershram66@gmail.com>
2023-06-17 21:17:45 +0300

    Todos list

--- react-redux-toolkit/src/components/Main.jsx
+++ react-redux-toolkit/src/components/Main.jsx
@@ -13,7 +13,7 @@ function Main() {
       setTodoList([
         ...todoList,
         {
-          id: new Date().toISOString(),
+          id: Date.now(),
           textInput,
           completed: false,
         }
@@ -22,6 +22,28 @@ function Main() {
     }
   }
 
+  const handleChange = (todoId) => {
+    setTodoList(
+      todoList.map(todo => {
+        if(todo.id !== todoId) return todo;
+        return {
+          ...todo,
+          completed: !todo.completed,
+        }
+      }
+    ))
+  }
+
+  const removeTodo = (todoId) => {
+    setTodoList(
+      todoList.filter(todo => todo.id !== todoId)
+    )
+  }
+
+  const getTime = (timeStamp) => {
+    const date= new Date(timeStamp);
+    return date.toDateString() + ', ' + date.getHours() + ":" + date.getMinutes() + ':' + date.getSeconds();
+  }
 
   return (
     <main>
@@ -44,9 +66,20 @@ function Main() {
           {
             todoList.map(todoItem => (
               <li key={todoItem.id} className="todo-item">
-                <input type="checkbox"/>
-                <div className="todo-item__text">{todoItem.textInput}</div>
-                <div className="todo-item__delete">&times;</div>
+                <input
+                  type="checkbox"
+                  className="todo-item__checkbox"
+                  checked={todoItem.completed}
+                  onChange={() => handleChange(todoItem.id)}
+                />
+                <div className="todo-item__text" title={todoItem.textInput}>{todoItem.textInput}</div>
+                <div className="todo-item__date">{getTime(todoItem.id)}</div>
+                <div
+                  className="todo-item__delete"
+                  onClick={() => removeTodo(todoItem.id)}
+                >
+                  &times;
+                </div>
               </li>
             ))
           }
