757d6b981a0adf16a02b4400fea3db850cd1178b
sergey <sershram66@gmail.com>
2023-06-23 18:59:07 +0300

    Added redux-persist

--- react-redux-toolkit/src/store/index.js
+++ react-redux-toolkit/src/store/index.js
@@ -1,8 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import todoReducer from "./todoSlice";
 
-export default configureStore({
-  reducer: {
-    todos: todoReducer,
-  },
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+  todos: todoReducer,
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store);
